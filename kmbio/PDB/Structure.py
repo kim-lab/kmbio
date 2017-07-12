594c3a94353c2bae8bc00e350a4247861188aad2
ostrokach <ostrokach@gmail.com>
2017-07-12 18:58:18 -0400

    Add an outline of the `to_dataframe` method

--- kmbio/PDB/Structure.py
+++ kmbio/PDB/Structure.py
@@ -4,6 +4,8 @@
 # as part of this package.
 
 """The structure class, representing a macromolecular structure."""
+import numpy as np
+import pandas as pd
 
 from kmbio.PDB.Entity import Entity
 
@@ -59,3 +61,19 @@ class Structure(Entity):
         for model_id in model_ids:
             structure.add(self[model_id].copy())
         return structure
+
+    def to_dataframe(self) -> (pd.DataFrame, np.ndarray):
+        """Convert this structure into a pandas DataFrame
+
+        The output of this method is intended to be compatible
+        with the :mdtraj:`Topology.from_dataframe` method.
+
+        Returns
+        -------
+        atoms : pandas.DataFrame
+            The atoms in the structure, represented as a data frame.
+        bonds : np.ndarray
+            The bonds in this structure, represented as an n_bonds x 2 array
+            of the indices of the atoms involved in each bond.
+        """
+        raise NotImplementedError
