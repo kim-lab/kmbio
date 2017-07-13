a4275919fd2ae107cae1fdb217dd4a7d8c028b8e
ostrokach <ostrokach@gmail.com>
2017-07-13 15:59:49 -0400

    "Release 1.69.4.dev0"

--- kmbio/__init__.py
+++ kmbio/__init__.py
@@ -10,7 +10,7 @@ of freely available Python tools for computational molecular biology.
 http://biopython.org
 """
 
-__version__ = "1.69.3"
+__version__ = "1.69.4.dev0"
 
 
 class MissingExternalDependencyError(Exception):
