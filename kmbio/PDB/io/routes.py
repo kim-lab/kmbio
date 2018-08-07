01b0281a6e2598768a649f5e818836e15626ee3b
Alexey Strokach <ostrokach@gmail.com>
2018-08-06 21:36:53 -0400

    Don't start routes functions with an underscore (they may be useful to outside code)

--- kmbio/PDB/io/routes.py
+++ kmbio/PDB/io/routes.py
@@ -1,7 +1,7 @@
 from collections import OrderedDict
 
 
-def _get_rcsb_url(pdb_id, pdb_type):
+def get_rcsb_url(pdb_id, pdb_type):
     if pdb_type in ["pdb", "cif"]:
         URL = "http://files.rcsb.org/download/{pdb_id}.{pdb_type}.gz"
     elif pdb_type in ["mmtf"]:
@@ -12,7 +12,7 @@ def _get_rcsb_url(pdb_id, pdb_type):
     return url
 
 
-def _get_ebi_url(pdb_id, pdb_type):
+def get_ebi_url(pdb_id, pdb_type):
     if pdb_type in ["mmtf"]:
         raise NotImplementedError("This route does not support '{}' file format!".format(pdb_type))
     URL = "http://www.ebi.ac.uk/pdbe/entry-files/download/{pdb_filename}"
@@ -21,7 +21,7 @@ def _get_ebi_url(pdb_id, pdb_type):
     return url
 
 
-def _get_wwpdb_url(pdb_id, pdb_type):
+def get_wwpdb_url(pdb_id, pdb_type):
     if pdb_type in ["mmtf"]:
         raise NotImplementedError("This route does not support '{}' file format!".format(pdb_type))
     URL = "ftp://ftp.wwpdb.org/pub/pdb/data/structures/divided/{pdb_format}/{pdb_filename}"
@@ -29,11 +29,11 @@ def _get_wwpdb_url(pdb_id, pdb_type):
     pdb_filename = {
         "pdb": "{pdb_id_middle}/pdb{pdb_id}.ent.gz",
         "cif": "{pdb_id_middle}/{pdb_id}.cif.gz",
-    }[pdb_type].format(
-        pdb_id_middle=pdb_id[1:3], pdb_id=pdb_id)
+    }[pdb_type].format(pdb_id_middle=pdb_id[1:3], pdb_id=pdb_id)
     url = URL.format(pdb_format=pdb_format, pdb_filename=pdb_filename)
     return url
 
 
-DEFAULT_ROUTES = OrderedDict([("rcsb://", _get_rcsb_url), ("ebi://", _get_ebi_url),
-                              ("wwpdb://", _get_wwpdb_url)])
+DEFAULT_ROUTES = OrderedDict(
+    [("rcsb://", get_rcsb_url), ("ebi://", get_ebi_url), ("wwpdb://", get_wwpdb_url)]
+)
