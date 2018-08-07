7804b98cb43934ac64123aaa36c0ebaee896c60a
ostrokach <ostrokach@gmail.com>
2018-08-07 13:21:01 -0400

    Format with black

--- setup.py
+++ setup.py
@@ -3,36 +3,39 @@ from Cython.Build import cythonize
 from setuptools import Extension, setup
 
 PACKAGES = [
-    'kmbio',
-    'kmbio.PDB',
-    'kmbio.PDB.core',
-    'kmbio.PDB.io',
-    'kmbio.PDB.parsers',
-    'kmbio.PDB.tools',
-    'kmbio.PDB.tools.QCPSuperimposer',
-    'kmbio.SVDSuperimposer',
-    'kmbio.KDTree',
+    "kmbio",
+    "kmbio.PDB",
+    "kmbio.PDB.core",
+    "kmbio.PDB.io",
+    "kmbio.PDB.parsers",
+    "kmbio.PDB.tools",
+    "kmbio.PDB.tools.QCPSuperimposer",
+    "kmbio.SVDSuperimposer",
+    "kmbio.KDTree",
 ]
 
 EXTENSIONS = [
     Extension(
-        'kmbio.KDTree._CKDTree', ["kmbio/KDTree/KDTree.c", "kmbio/KDTree/KDTreemodule.c"],
-        include_dirs=[np.get_include()]),
+        "kmbio.KDTree._CKDTree",
+        ["kmbio/KDTree/KDTree.c", "kmbio/KDTree/KDTreemodule.c"],
+        include_dirs=[np.get_include()],
+    ),
     Extension(
-        'kmbio.PDB.tools.QCPSuperimposer.qcprotmodule',
+        "kmbio.PDB.tools.QCPSuperimposer.qcprotmodule",
         ["kmbio/PDB/tools/QCPSuperimposer/qcprotmodule.c"],
-        include_dirs=[np.get_include()]),
-    Extension('*', ["kmbio/PDB/parsers/*.pyx"]),
+        include_dirs=[np.get_include()],
+    ),
+    Extension("*", ["kmbio/PDB/parsers/*.pyx"]),
 ]
 
 setup(
-    name='kmbio',
+    name="kmbio",
     version="2.0.6",
-    author='The Biopython Contributors + KimLab',
-    author_email='alexey.strokach@kimlab.org',
-    url='https://github.com/kimlaborg/kmbio',
+    author="The Biopython Contributors + KimLab",
+    author_email="alexey.strokach@kimlab.org",
+    url="https://github.com/kimlaborg/kmbio",
     description="Freely available tools for computational molecular biology.",
-    download_url='https://github.com/kimlaborg/kmbio/release/',
+    download_url="https://github.com/kimlaborg/kmbortio/release/",
     packages=PACKAGES,
     ext_modules=cythonize(EXTENSIONS),
     package_data={},
