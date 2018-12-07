dde1fd48016bd6a16c958d61b4676c4b0c1ed9a5
ostrokach <ostrokach@gmail.com>
2018-12-06 20:07:00 -0500

    Export guess_pdb_id, guess_pdb_type, get_parser to global package namespace

--- kmbio/PDB/io/__init__.py
+++ kmbio/PDB/io/__init__.py
@@ -1,4 +1,4 @@
 from .routes import DEFAULT_ROUTES
-from .loaders import load
+from .loaders import load, guess_pdb_id, guess_pdb_type, get_parser
 from .savers import PDBIO, Select, save
 from .viewers import structure_to_ngl, view_structure
